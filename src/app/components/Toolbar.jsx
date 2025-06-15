67f38065b783dbdf21d9d1cda5a47b57197cbadf
Kaimond <kw299@student.le.ac.uk>
2025-06-15 23:59:22 +0100

    Added further styling to toolbar

--- src/app/components/Toolbar.jsx
+++ src/app/components/Toolbar.jsx
@@ -4,7 +4,7 @@ import { FaPencilAlt } from "react-icons/fa";
 import { FaEraser } from "react-icons/fa";
 
 export default function Toolbar({ clearCanvas, setColour, setWidth, eraser, drawing, setBackgroundColour }) {
-    const [activeButton, setActiveButton] = useState("draw");
+    const [activeTool, setActiveTool] = useState("draw");
     const [selectedColour, setSelectedColour] = useState("#ffffff");
     const [lineWidth, setLineWidth] = useState("5");
     const [backgroundPickerColour, setBackgroundPickerColour] = useState("#000000");
@@ -28,7 +28,7 @@ export default function Toolbar({ clearCanvas, setColour, setWidth, eraser, draw
     };
 
     return (
-        <div style={{ marginBottom: "10px" }}>
+        <div style={{ marginBottom: "10px", backgroundColor: "#BEBEBE", color: "black", width: "700px", borderRadius: "10px" }}>
             <button
                 onClick={clearCanvas}
                 style={{
@@ -55,25 +55,25 @@ export default function Toolbar({ clearCanvas, setColour, setWidth, eraser, draw
                 style={{ marginLeft: "4px", border: "2px solid white", verticalAlign: "middle" }}
             />
             <button
-                onClick={() => { drawing(); setActiveButton("draw") }}
+                onClick={() => { drawing(); setActiveTool("draw") }}
                 style={{
                     padding: "8px",
                     marginRight: "10px",
                     cursor: "pointer",
-                    border: activeButton === "draw" ? "2px solid #6C3BAA" : "",
-                    borderRadius: activeButton === "draw" ? "12px" : "",
+                    border: activeTool === "draw" ? "2px solid #6C3BAA" : "",
+                    borderRadius: activeTool === "draw" ? "12px" : "",
                 }}
             >
                 <FaPencilAlt />
             </button>
             <button
-                onClick={() => { eraser(); setActiveButton("erase") }}
+                onClick={() => { eraser(); setActiveTool("erase") }}
                 style={{
                     padding: "8px",
                     marginRight: "10px",
                     cursor: "pointer",
-                    border: activeButton === "erase" ? "2px solid #6C3BAA" : "",
-                    borderRadius: activeButton === "erase" ? "12px" : "",
+                    border: activeTool === "erase" ? "2px solid #6C3BAA" : "",
+                    borderRadius: activeTool === "erase" ? "12px" : "",
                 }}
             >
                 <FaEraser />
