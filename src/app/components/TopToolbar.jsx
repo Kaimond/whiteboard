a7a815bddd443f634c8d406d32747f1e4941bbaa
Kaimond <kw299@student.le.ac.uk>
2025-06-22 08:36:59 +0100

    Moved clear canvas, undo, and redo buttons to the top toolbar

--- src/app/components/TopToolbar.jsx
+++ src/app/components/TopToolbar.jsx
@@ -1,9 +1,12 @@
 "use client";
 import React from 'react'
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { FaTrash, FaUndo, FaRedo, FaEye, FaEyeSlash } from "react-icons/fa";
 import styles from "../Toolbar.module.css"
 
 export default function TopToolbar({
+    clearCanvas,
+    undo,
+    redo,
     showToolbar,
     setShowToolbar
 }) {
@@ -15,6 +18,32 @@ export default function TopToolbar({
 
     return (
         <div className={styles.topToolbar}>
+            <div className={styles.tooltip} data-tooltip="Clear (Ctrl + Del)">
+                <button
+                    className={styles.button}
+                    onClick={clearCanvas}
+                >
+                    <FaTrash />
+                </button >
+            </div>
+
+            <div className={styles.tooltip} data-tooltip="Undo (Ctrl + Z)">
+                <button
+                    className={styles.button}
+                    onClick={undo}
+                >
+                    <FaUndo />
+                </button>
+            </div>
+            <div className={styles.tooltip} data-tooltip="Redo (Ctrl + Shift + Z)">
+                <button
+                    className={styles.button}
+                    onClick={redo}
+                >
+                    <FaRedo />
+                </button>
+            </div>
+
             <button
                 className={styles.button}
                 onClick={toggleToolbar}
